fd783d50ba0550fc9dad755378db53f8a9ed66f5
Mouad <kawmimouad06@gmail.com>
2026-02-11 21:09:02 +0100

    Fix useEffect import error

--- src/components/Product/ProductDetails.jsx
+++ src/components/Product/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { ListChecks, MessageSquare, Info } from 'lucide-react';
 import Breadcrumbs from '../Layout/Breadcrumbs';
 
